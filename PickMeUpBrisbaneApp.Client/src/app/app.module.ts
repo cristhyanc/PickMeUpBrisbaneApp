29d7fefb1c0522ed2104137ba4d7abba36df5c4c
Cristhyan Cardona <cristhyan@outlook.com>
2017-09-09 23:11:42 +1000

    Sprint 0

--- PickMeUpBrisbaneApp.Client/src/app/app.module.ts
+++ PickMeUpBrisbaneApp.Client/src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { BookingServiceComponent } from './booking-service/booking-service.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'angular-calendar';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    LogInComponent,
+    BookingServiceComponent,
+    HomeComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
+    AlertModule.forRoot(),
+    AppRoutingModule,
+     BrowserAnimationsModule,
+    CalendarModule.forRoot(),
+      RouterModule.forRoot([
+      { path: '', redirectTo: 'Home', pathMatch: 'full' },
+      { path: 'Home', component: HomeComponent},
+      { path: 'Login', component: LogInComponent},
+      { path: 'booking', component: BookingServiceComponent},
+      { path: 'booking/:bookId', component: BookingServiceComponent},
+      { path: '**', component: NotFoundPageComponent}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
